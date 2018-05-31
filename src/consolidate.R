d94352d1195eee3d742f82175243aec4bb55c992
Victor Ordu <victorordu@outlook.com>
2018-05-31 19:17:29 +0100

    Consolidation successful

--- src/consolidate.R
+++ src/consolidate.R
@@ -25,11 +25,12 @@ pause <- function() {
 ## Import data
 # TODO: Examine this path
 if (!exists("dbcon")) {
-    dbcon <- dbConnect(SQLite(), path_to_db)
-    tabl <- grep("NARC_mail", dbListTables(dbcon), value = TRUE)
-    df <- dbReadTable(dbcon, tabl)
-    dbDisconnect(dbcon)
-    rm(dbcon)
+  dbcon <- dbConnect(SQLite(), path_to_db)
+  tabl <- grep("NARC_mail", dbListTables(dbcon), value = TRUE)
+  df <- dbReadTable(dbcon, tabl)
+  dbDisconnect(dbcon)
+  rm(dbcon)
+  df <- as_tibble(df)
 }
 
 cat("\nOverview of the data:\n")
@@ -38,23 +39,56 @@ glimpse(df)
 cat("\nNext: Check identifier variables for duplications\n")
 pause()
 invisible(df %>%
-              select(name, phone, email) %>%
-              {
-                  cat("* Level of duplication:\n")
-                  sapply(colnames(.), function(x) {
-                      cat("** Column",
-                          sQuote(x),
-                          "has",
-                          sum(duplicated(.[[x]]), na.rm = TRUE),
-                          "duplications\n")
-                  })
-              }
-          )
+            select(name, phone, email) %>%
+            {
+              cat("* Level of duplication:\n")
+              sapply(colnames(.), function(x) {
+                cat("** Column",
+                    sQuote(x),
+                    "has",
+                    sum(duplicated(.[[x]]), na.rm = TRUE),
+                    "duplications\n")
+              })
+            })
 cat("\nNext: Aggregate the duplicated values\n")
 pause()
 
-cat("* Summary of data grouped by 'name':\n")
-df %>%
-    group_by(name) %>%
-    summarise_all(first) %>% 
-    print()
+cat("* Sort the data frame by 'name':\n")
+arr <- df %>%
+  arrange(name) %>% 
+  select(-serialno) %>% 
+  print()
+
+
+cat("\nNext: Attempt to fill in missing values\n")  # use greedy algorithm
+pause()
+
+## List unique names
+uniq <- unique(arr$name)
+
+## Find repeated names and associated records
+cons <- map_dfr(uniq, function(N) {
+  index <- which(arr$name == N)
+  if (length(index > 1)) {
+    sprintf("* Merging available records for '%s':\n", N)
+    merg <- arr %>% 
+      filter(name %in% N) %>% 
+      map_dfc(function(x) {
+        U <- unique(x)
+        
+        ## where there is more than one distinct 
+        ## value, we present the user with options
+        if (length(U) > 1) {
+          picked <- menu(choices = U, title = "Choose one of the following:")
+          val <- U[picked]
+        }
+        else {
+          val <- U
+        }
+        val
+      })
+  }
+})
+
+
+# TODO: After consolidation, carry out some integrity checks...
--- src/harmonise.R
+++ src/harmonise.R
@@ -1,6 +1,16 @@
 ## harmonise.R
 
-## Harmonising Various Spreadsheets into one Database
+## Harmonising various spreadsheets into one database
 if (!require(NARCcontacts))
-    devtools::install_github("DevSolutionsLtd/NARCcontacts")
-NARCcontacts::harmonise_narc_excel("workbooks/")
+  devtools::install_github("DevSolutionsLtd/NARCcontacts")
+if (!interactive()) {
+  args <- commandArgs(trailingOnly = TRUE)
+  if (!is.null(args))
+    workbooks <- args
+}
+workbooks <-
+  readline("Enter the path to the directory containing spreadsheets: ")
+if (!dir.exists(workbooks))
+  stop(sQuote(basename(workbooks)), " does not exist at this location.")
+
+NARCcontacts::harmonise_narc_excel(workbooks)
