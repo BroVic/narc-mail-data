d7a009966d604d03a4a334ee4012514bc84c033b
Victor Ordu <victorordu@outlook.com>
2018-08-23 20:15:25 +0100

    Moved this feature to the NARCcontacts package

--- src/consolidate.R
+++ src/consolidate.R
@@ -2,13 +2,7 @@
 
 ## Consolidate the database by merging repeated records
 ## ````````````````````````````````````````````````````
-
-pkgs <- c("RSQLite", "dplyr", "purrr", "rprojroot")
-lapply(pkgs, function(x)
-  suppressPackageStartupMessages(library(x, character.only = TRUE)))
-
-source('src/funs.R')
-source('src/check-data.R')
+library(NARCcontacts)
 
 ## Make relevant files discoverable in-project
 root <- is_rstudio_project
@@ -17,22 +11,5 @@ database <- "NARC-mailing-list.db"
 path_to_db <-
   find_root_file("data", database, criterion = criterion)
 
-## Load the data
-cat("* Overview of the data:\n")
-dat <- importDataFromDb(path_to_db, "NARC_mail") %>% 
-  as_tibble() %>% 
-  print()
-
-checkIdVarDuplicates(dat)
-
-df_pre <- aggregateDuplicatedVals(dat)
-skip <-
-  menu(c('Yes', 'No'), t = "NEXT: Fill in missing values. Continue?") %>%
-  {
-    isTRUE(. == 2)
-  }
-if (!skip)
-  pause()
-df_post <- fillMissingVals(df_pre, skip = skip)
-checkDataIntegrity(df_pre, df_post, skip = skip)
-storeConsolidatedData(df_post, path_to_db)
+## Start main operation
+consolidate_narc_mail(path_to_db)
