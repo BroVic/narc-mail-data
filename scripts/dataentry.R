22d12e5fdce787aad5acee9c7486e6e123bcbc4f
Victor Ordu <victorordu@outlook.com>
2019-06-17 21:16:21 +0100

    Refine package loading

--- scripts/dataentry.R
+++ scripts/dataentry.R
@@ -2,8 +2,10 @@
 
 ## Upon successful execution, this script will open a data entry form.
 
-if (!requireNamespace("DataEntry"))
-  install.packages(c("RGtk2", "gWidgets2", "gWidgets2RGtk2", "DataEntry"),
-                   repos = "https://cran.rstudio.com")
+if (!requireNamespace("DataEntry")) {
+  dep <- c("RGtk2", "gWidgets2", "gWidgets2RGtk2")
+  isMissing <- isFALSE(dep %in% .packages(all.available = TRUE))
+  install.packages(c(dep[isMissing], "DataEntry"), repos = "https://cran.rstudio.com")
+}
 
 DataEntry::DataEntry()
