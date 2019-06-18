c430e2b3d8d628420bc16168d1d2e2c3515f56de
Victor Ordu <victorordu@outlook.com>
2019-06-18 17:22:08 +0100

    General improvements

--- scripts/update.R
+++ scripts/update.R
@@ -2,7 +2,7 @@
 
 cat("Attaching required packages... ")
 suppressPackageStartupMessages(
-  x <- lapply(c("here", "NARCcontacts", "RSQLite", "tidyverse"), 
+  x <- lapply(c("RSQLite", "tidyverse"), 
          function(x) {
            invisible(library(x, character.only = TRUE, quietly = TRUE))
          }
@@ -12,24 +12,22 @@ cat("Done.\n")
 
 arg <- commandArgs(trailingOnly = TRUE)
 
-if (interactive() && identical(getwd(), here())) {
-  deProjFile <- "data/dataentry-sample.dte"
-} else if (length(args) != 1) {
-  stop("Usage: Rscript <scriptname> <DataEntry project>")
+if (interactive()) {
+  deProjFile <- file.choose()
+} else {
+  if (length(arg) == 1L)
+    deProjFile <- arg
+  else
+    stop("Usage: Rscript <scriptname> <DataEntry project>")
 }
 
-deProjFile <- arg
 if (!file.exists(deProjFile)) {
-  stop(sQuote(deProjFile), " does not exist")
+  stop(sQuote(deProjFile), "does not exist")
 }
 
 # Create self-contained space for the incoming data
 databox <- new.env()
 load(deProjFile, databox, verbose = TRUE)
-
-newData <- databox$Data %>% 
-  select(-id)             # remove DataEntry::DataEntry()'s auto-numbering
-
 
 # ------------------------------------------------------------------------
 # Database operations
@@ -38,7 +36,9 @@ con <- dbConnect(SQLite(), "data/NARC-mailing-list.db")
 try({
   
   oldData <- dbReadTable(con, name = "mail_consolid")
-
+  newData <- databox$Data %>% 
+    select(-id)             # remove DataEntry::DataEntry()'s auto-numbering
+  
   if (!identical(colnames(oldData), colnames(newData))) {
     stop("Column mismatch between incoming and existing datasets")
   }
@@ -47,14 +47,14 @@ try({
     bind_rows(newData)
   
   numNewData <- nrow(newData)
-  if (nrow(oldData) + numNewData != nrow(updatedData)) {
+  if ((nrow(oldData) + numNewData) != nrow(updatedData)) {
     
     warning("The sum of observations from both datasets is incorrect")
     
     opt <- menu(c("Yes", "No"), title = "Continue?")
     
     if (identical(opt, 2L))
-      stop()
+      stop("Operation was stopped")
   }
   
   cat("Writing new records to local database...")
@@ -64,26 +64,27 @@ try({
                overwrite = TRUE)
   cat(sprintf("%d new records added to the database\n", numNewData))
   
+  databox$Data <- newData[FALSE, ]
+  databox$Data <- databox$Data %>% 
+    {
+      bind_cols(data.frame(id = integer()), .)  # return id column
+    }
+  
+  # ---- Caching -----------------------------------------------------
+  cache <- normalizePath("data/.cache")
+  
+  if (!dir.exists(cache))
+    dir.create(cache)
+  
+  tmp <- tempfile("cache", cache, ".rds")
+  saveRDS(newData, tmp)
+  save(list = ls(envir = databox), file = deProjFile, envir = databox)
+  
+  cat("Data entry file has been reset and backup cached at",
+      dirname(tmp),
+      fill = TRUE)
+  
 })
+
 dbDisconnect(conn = con)
-# ------------------------------------------------------------------------
-
-
-cat("Caching... \n")
-tmp <- tempfile()
-saveRDS(newData, tmp)
-cat(
-  sprintf("Cache witn %d records stored in .RDS format at %s", numNewData, tmp),
-  file = "log.txt",
-  fill = TRUE
-)
-
-
-cat("Resetting the records in the data entry project file.\n")
-databox$Data <- newData[FALSE, ]
-databox$Data <- databox$Data %>% 
-  {
-    bind_cols(data.frame(id = integer()), .)  # return id column
-  }
-save(list = ls(envir = databox), file = deProjFile, envir = databox)
-# DONE
+#END
