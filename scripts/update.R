95c8f58ef17b79f4676a1f3c7caecafe23b8c441
Victor Ordu <victorordu@outlook.com>
2019-06-20 19:05:24 +0100

    Abstraction refactoring

--- scripts/update.R
+++ scripts/update.R
@@ -1,113 +1,30 @@
 # Update the local database using records stored in the .DTE file
 
-cat("Attaching required packages... ")
-suppressPackageStartupMessages(
-  x <- lapply(c("RSQLite", "tidyverse"), 
-         function(x) {
-           invisible(library(x, character.only = TRUE, quietly = TRUE))
-         }
-  )
-)
-cat("Done.\n")
+source(file.path(here::here(), "scripts", "helpers.R"))
+attach_packages()
 
-arg <- commandArgs(trailingOnly = TRUE)
+dataEntryFile <- fetch_entered_data()
 
-if (interactive()) {
-  dataEntryFile <- file.choose()
-} else {
-  if (length(arg) == 1L)
-    dataEntryFile <- arg
-  else
-    stop("Usage: Rscript <scriptname> <DataEntry project>")
-}
+selfContainedSpace <- new.env()
+load(dataEntryFile, selfContainedSpace, verbose = TRUE)
 
+enteredData <- selfContainedSpace$Data %>% select(-id)
 
-# Create self-contained space for the incoming data
-databox <- new.env()
-load(dataEntryFile, databox, verbose = TRUE)
+dtFile <- select_db_or_object()
 
+if (!(isDb <- endsWith(dtFile, ".db")) &
+    !(isRds <- endsWith(dtFile, ".rds")))
+  stop("File format is not supported")
 
-# ------------------------------------------------------------------------
-# Database operations
+if (isDb)
+  work_with_database(dtFile, enteredData) else if (isRds)
+    work_with_rds(dtFile, enteredData)
 
+reset_dte_file(dataEntryFile, enteredData, selfContainedSpace)
 
-newData <- databox$Data %>% 
-  select(-id)             # remove DataEntry::DataEntry()'s auto-numbering
-
-cat("Select the database to be updated\n")
-databaseFile <- file.choose()
-
-con <- dbConnect(SQLite(), databaseFile)
-
-
-try ({
+temp <- cacheData(enteredData)
   
-  tableName <- "NARC_consolidated"
-  
-  if (isDbFile <- endsWith(databaseFile, ".db")) {
-    oldData <-
-      dbReadTable(con, name = ) # TODO: Fetch table name generically
-    
-  } else if (endsWith(databaseFile, ".rds")) {
-    oldData <- readRDS(databaseFile)
-    
-  }
-  
-  # TODO: Undo this hard-coding later
-  oldData <- oldData %>% 
-    select(-serialno)
-  
-  if (!identical(colnames(oldData), colnames(newData))) {
-    stop("Column mismatch between incoming and existing datasets")
-  }
-  
-  updatedData <- oldData %>%
-    bind_rows(newData)
-  
-  numNewData <- nrow(newData)
-  if ((nrow(oldData) + numNewData) != nrow(updatedData)) {
-    warning("The sum of observations from both datasets is incorrect")
-    
-    opt <- menu(c("Yes", "No"), title = "Continue?")
-    
-    if (identical(opt, 2L))
-      stop("Operation was stopped")
-  }
-  
-  if (isDbFile) {
-    cat("Writing new records to local database...")
-    dbWriteTable(con,
-                 name = tableName,
-                 value = updatedData,
-                 overwrite = TRUE)
-    cat(sprintf("%d new records added to the database\n", numNewData))
-  } 
-  else {
-    saveRDS(updatedData, file = databaseFile)
-  }
-  
-  # Reset the data entry project file
-  databox$Data <- newData[FALSE, ]
-  databox$Data <- databox$Data %>% 
-  {
-    bind_cols(data.frame(id = integer()), .)  # return id column
-  }
-  save(list = ls(envir = databox), file = dataEntryFile, envir = databox)
-  
-  # ---- Caching -----------------------------------------------------
-  cache <- normalizePath("data/.cache")
-  
-  if (!dir.exists(cache))
-    dir.create(cache)
-  
-  tmp <- tempfile("cache", cache, ".rds")
-  saveRDS(newData, tmp)
-  
-  cat("Data entry file has been reset and backup cached at",
-      dirname(tmp),
-      fill = TRUE)
-})
-
-dbDisconnect(conn = con)
-
+cat("Data entry file has been reset and backup cached at",
+    dirname(temp),
+    fill = TRUE)
 #END
